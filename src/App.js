4b81056b03de556287ee14feb3fcbf5a65553313
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-09 19:30:14 -0800

    app component set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import styles from './App.module.scss'
+
+//components
+
+import Header from './components/Header.js';
+import Todos from './components/Todos.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Header />
+      <Todos />
+      <Footer />
     </div>
   );
 }
